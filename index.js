908606f9432f765599e49957e71a1c98f3a0c8ed
kuruvasaikiran11 <kuruvasaikiran11@gmail.com>
2023-09-01 12:34:54 +0530

    Designed Footer

---
+++ index.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+    // Corousel
+    $('.center').slick({
+        centerMode: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        dots: true,
+        arrows: false,
+        responsive: [
+          {
+            breakpoint: 768,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 3
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 1
+            }
+          }
+        ]
+    });
+
+});
